d667fc315223dd04f50ab093103d26332d790033
Wangbaoqi <wangbaoqi8839@gmail.com>
2023-08-02 17:32:49 +0800

    feat: rebuild new package and test

--- react-demo/src/main.tsx
+++ react-demo/src/main.tsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: '/jsx',
+        element: <div>jsx</div>
+      },
+      {
+        path: '/class-component',
+        element: <div>classComponent</div>
+      }
+    ]
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
